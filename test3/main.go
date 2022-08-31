a5d8b822da9ece8d4457fb32b21ec735ef47c82d
mazxccc <mazxccc@gmail.com>
2022-09-01 02:56:08 +0300

    #feature/jirakey6 #done #m6

--- test3/main.go
+++ test3/main.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Println(len("Test Message"))
+	fmt.Println("Test Message"[3])
+	fmt.Println("Test Message" + " Another message")
+
 }
